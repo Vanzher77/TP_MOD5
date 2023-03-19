2149263917ad132d1cfbbe3035ae3a24b2c97c22
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-03-19 19:13:43 +0700

    Menambahkan Class DataGeneric

--- tpmodul5_1302213058/tpmodul5_1302213058/Program.cs
+++ tpmodul5_1302213058/tpmodul5_1302213058/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        //DataGeneric menggunakan generic bertipe integer karena menggunakan angka dalam menginput data
+        DataGeneric<int> dat = new DataGeneric<int>(1302213058);
+        dat.printData();
+    }
+
+}
+
+
+//class DataGeneric menggunakan Generic T 
+class DataGeneric<T>
+{
+    //mendefinisikan generic T dengan variable Data
+    private T Data;
+
+    public DataGeneric(T Data)
+    {
+        this.Data = Data;
+    }
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
